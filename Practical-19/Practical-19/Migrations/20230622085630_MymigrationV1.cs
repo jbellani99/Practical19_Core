7333dfd690d1f808841deae368556e87d1fd6fe7
Jayesh bellani <54457951+jbellani99@users.noreply.github.com>
2023-06-23 09:06:43 +0530

    Completed Practical-19

---
+++ Practical-19/Practical-19/Migrations/20230622085630_MymigrationV1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Practical_19.Migrations
+{
+    /// <inheritdoc />
+    public partial class MymigrationV1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
